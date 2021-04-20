acee143fa3ba8e42c3e3834b0a41d69aa40d71c1
sarosh2 <41180340+sarosh2@users.noreply.github.com>
2021-04-20 19:59:33 +0500

    Add files via upload

---
+++ OS/src/impl/kernel/main.c
@@ -0,0 +1,11 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str(" ______    ______   ______   ______    ______ \n");
+    print_str("|      |  |        |        |      |  |      |\n");
+    print_str("|______   |______  |______  |         |______ \n");
+    print_str("       |  |        |        |                |\n");
+    print_str("|______|  |______  |______  |______|  |______|");
+}
